3bccdcf7b766885586b2e5aca1e8d251e6d0264e
Tara <tararee@gmail.com>
2016-08-01 15:28:47 -0400

    Added heroku, not fully functioning

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 $(document).ready(function() {
 	var WorkoutLog = (function($, undefined) {
-		var API_BASE = "//workoutapi-name-tararee.herokuapp.com/api/";
-
+		// var API_BASE = location.hostname === "localhost//workoutapi-name-tararee.herokuapp.com/api/";
+		var API_BASE = location.hostname === "localhost" ?
+			"//localhost:3000/api/" : 
+			"//workoutapi-name-tararee.herokuapp.com/api/";
 		var setAuthHeader = function(sessionToken) {
 			window.localStorage.setItem("sessionToken", sessionToken);
 			
--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
+var watch = require('gulp-watch');
 
 var javascriptFiles = [
 	'./app.js',
@@ -11,9 +13,15 @@ var javascriptFiles = [
 
 gulp.task('bundle', function() {
 	return gulp.src(javascriptFiles)
+		.pipe(sourcemaps.init())
 		.pipe(concat('bundle.js')) // squash files together into bundle.js
 		.pipe(uglify())
+		.pipe(sourcemaps.write('./maps/'))
 		.pipe(gulp.dest('./dist'));// save into /dist folder
 });
 
-gulp.task('default', ['bundle']);
+gulp.task('watch', function() {
+	gulp.watch(javascriptFiles, ['bundle']);
+});
+
+gulp.task('default', ['bundle', 'watch']);
